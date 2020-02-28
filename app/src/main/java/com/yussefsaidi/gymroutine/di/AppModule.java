22d3d46eb6eeef32d3702d31e32877724faa96fc
Yussef <saidi.yussef@live.fr>
2020-02-27 19:33:20 -0500

    Provide Retrofit instance

--- app/src/main/java/com/yussefsaidi/gymroutine/di/AppModule.java
+++ app/src/main/java/com/yussefsaidi/gymroutine/di/AppModule.java
@@ -6,14 +6,26 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 import com.yussefsaidi.gymroutine.R;
+import com.yussefsaidi.gymroutine.util.Constants;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class AppModule {
+
+    @Singleton
+    @Provides
+    static Retrofit provideRetrofitInstance(){
+        return new Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
 
     @Singleton
     @Provides
